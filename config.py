a8a1427391f090a125e65ad9b0cc0cbb125fbdfd
Sandeep sharma <rs3268840@gmail.com>
2026-02-17 00:25:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # -- Git / updates --------------------------------------------------------------
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Suraj08832/siyamedia")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
 # -- Support links --------------------------------------------------------------
